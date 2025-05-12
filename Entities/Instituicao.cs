62e11eae59530f7e33dfb87138be6fd874e0e60a
Rafael-Del-Vaz <rafinha68taqua@gmail.com>
2025-05-12 20:55:22 -0300

    Atualização do codigo

--- Entities/Instituicao.cs
+++ Entities/Instituicao.cs
@@ -1,4 +1,14 @@
 abstract class Instituicao{
+    public void ShowAMS(){
+    Console.WriteLine($"Nome do aluno: {NomeAluno}");
+    Console.WriteLine($"Ano da matrícula: {AnoMatricula}");
+    Console.WriteLine($"Horas do curso: {HorasCurso}");
+    }
+    public void ShowTecnico(){
+    Console.WriteLine($"Nome do aluno: {NomeAluno}");
+    Console.WriteLine($"Ano da matrícula: {AnoMatricula}");
+    Console.WriteLine($"Horas do curso: {HorasCurso}");
+    }
     
     public string? NomeAluno {get; protected set;}
     public int AnoMatricula {get; protected  set;}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Digite o seu nome");
-string Nome = (Console.ReadLine());
+string? Nome = (Console.ReadLine());
 
 Console.WriteLine("Digite o ano da sua matricula");
 int Ano = int.Parse(Console.ReadLine());
@@ -13,19 +13,16 @@ EtecTecnico TECNICO = new EtecTecnico(Nome, Ano, Horas);
 Console.WriteLine("Escolha uma Etec (TECNICO ou AMS)");
 string Etec = (Console.ReadLine());
 
-if (Etec.ToLower() == "ams")
+if (Etec?.ToLower() == "ams")
 {
-    Console.WriteLine("Nome dos alunos: " + AMS.NomeAluno);
-    Console.WriteLine("Ano da matricula: " + AMS.AnoMatricula);
-    Console.WriteLine("Horas do curso: " + AMS.HorasCurso);
-    Console.WriteLine($"Apenas os alunos do AMS tem a obrigatoriadade de completar {AMS.HorasObrigatorias} horas no curso ");
+    AMS.ShowAMS();
+    Console.WriteLine($"Apenas os alunos do AMS têm a obrigatoriedade de completar {AMS.HorasObrigatorias} horas no curso.");
+
 }
 else
-if (Etec.ToLower() == "tecnico")
+if (Etec?.ToLower() == "tecnico")
 {
-    Console.WriteLine("Nome dos alunos: " + TECNICO.NomeAluno);
-    Console.WriteLine("Ano da matricula: " + TECNICO.AnoMatricula);
-    Console.WriteLine("Horas do curso: " + TECNICO.HorasCurso);
+    TECNICO.ShowTecnico();
 }
 else
 {
